4a84f299309d060d5c60a6d9d6b308aab7412d7e
TakedaSyunto <133711925+TakedaSyunto@users.noreply.github.com>
2023-05-19 00:51:51 +0900

    Add files via upload

---
+++ SelfIntroduction.java
@@ -0,0 +1,26 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	
+	 // 氏名
+	 String name = "山田 花子";
+	 // 年齢
+	 int age = 25;
+	 // 趣味
+	 String hobby = "映画鑑賞";
+
+
+	 /*
+	 * 自己紹介文を出力する処理
+	 */
+
+	 System.out.println("私の名前は" + name + "です。");
+	 System.out.println("年齢は" + age + "歳です。");
+	 System.out.println("趣味は" + hobby + "です。");
+		
+	}
+
+}

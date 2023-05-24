1ccc32ef3a055f28576810f1298e0057ddbb4749
TakedaSyunto <syuntotakeda1123@gmail.com>
2023-05-24 22:58:48 +0900

    Java1.3課題 完了致しました。

---
+++ HelloWorld_FamilynameFirstname/src/sample/Sample.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		//	Hello,Worldと表示
+		System.out.println("Hello,World");
+	}
+
+}
---
+++ HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+
+		// 氏名
+		String name = "山田 花子";
+
+		// 年齢
+		int age = 25;
+
+		// 趣味
+		String hobby = "映画鑑賞";
+
+		/*
+		 * 自己紹介文を出力
+		 */
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
